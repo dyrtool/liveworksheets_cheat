093d72a510ac4ed6ad17f3dfa626da3ad631a4ec
dyrtool <65508583+dyrtool@users.noreply.github.com>
2020-12-17 22:21:26 +0100

    remove debug features

--- cheat.js
+++ cheat.js
@@ -1,31 +1,18 @@
 var data = JSON.parse(contenidojson);
-
-
 var result = [];
 
 for(var i = 0, len = data.length; i < len; i++) {
-  var list = data[i];
-  
+  var list = data[i]
   result[i] = { name: list[0] };
 
   for(var j = list.length - 1; j >= 1; j = j - 2) {
     var key = list[j];
     var value = list[j - 1];
-    
-    if (j > 100) {
-        //console.log(j)
-        //console.log('calc', j, key, value);
-    }
-    
-
     result[i][key] = value;
-
-    
   }
 }
 
 var loops = 0;
-
 result.forEach(function(entry) {
     console.log(loops.toString() + ". " + entry.name);
     loops++;
